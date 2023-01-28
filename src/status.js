58a85191535877672b6f0c98479044841ae60584
Mladan Ilic <116818129+djo1975@users.noreply.github.com>
2023-01-28 14:06:21 +0100

    Delete status.js

--- src/status.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-cycle
-import { tasks } from './index.js';
-
-// eslint-disable-next-line import/prefer-default-export
-export const toggleTaskStatus = (taskIndex) => {
-  tasks[taskIndex].completed = !tasks[taskIndex].completed;
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-};
